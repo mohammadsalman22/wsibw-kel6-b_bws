36f61f900ab1eedd7658ed82b61586805c4deb0a
Tegar Ar Rasyiid Saifullah <71806452+TegarArRasyiid2020@users.noreply.github.com>
2020-10-30 22:58:03 +0700

    Add files via upload

---
+++ belajar_php_TegarArRasyiidSaifullah_e41191211/syntax_html_php.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Belajar PHP"?></title>
+</head>
+<body>
+    
+</body>
+      <?php
+           echo "Saya sedang belajar PHP<br>";
+           echo "<p>Belajar PHP hingga master</P";
+      ?>     
+ </body>          
+ </html>     
