ad9c06e659935eb72367fc5c8b6a9a44f92e2086
Erkan-s <72178769+Erkan-s@users.noreply.github.com>
2020-12-03 20:15:53 +0700

    Topic 11
    Workshop Sistem Informasi Berbasis Web Login Database

---
+++ E41191510_ErikRizkiFirdaus_Form Login/register.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Daftar</title>
+	<link rel="stylesheet" type="text/css" href="assets/font-aw/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+	<style type="text/css">
+		.alert{
+			position: relative;
+		    padding: .75rem 1.25rem;
+		    border: 1px solid transparent;
+		}
+		.alert-danger{
+		    color: #721c24;
+		    background-color: #f8d7da;
+		    border-color: #f5c6cb;
+		}
+		.alert-success{
+		    color: #155724;
+		    background-color: #d4edda;
+		    border-color: #c3e6cb;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+	<?php session_start();
+	if (isset($_SESSION['alert'])) {
+		if(isset($_SESSION['alert']['danger'])){
+			?>
+			<div class="alert alert-danger">
+				<?=$_SESSION['alert']['danger'];?>
+			</div>
+			<?php
+		}else{
+			?>
+			<div class="alert alert-success">
+				<?=$_SESSION['alert']['success'];?>
+			</div>
+			<?php
+		}
+	}
+		session_unset();
+		session_destroy(); 
+	?>
+			<div class="right">
+			<div class="head">
+				<h3>Daftar</h3>
+			</div>
+				<form id="box-login" method="POST" action="act_login.php" style="margin-top: 0!important" autocomplete="off">
+						<div class="user">
+							<input type="text" name="nama" placeholder="Name">
+						</div>
+						<div class="user">
+							<input type="text" name="username" placeholder="Username">
+						</div>
+						<div class="user1">
+							<input type="password" name="password" placeholder="Password">
+						</div>
+					<input type="hidden" name="aksi" value="daftar">
+					<input type="submit" value="Masuk" name="submit">
+					<a href="index.php">masuk</a>
+				</form>
+			</div>
+		
+		</div>
+</body>
+</html>
