68c9400d881bf211bfe3ee9dac4c74c2ce3ccb84
mohammadsalman22 <71873673+mohammadsalman22@users.noreply.github.com>
2021-01-07 23:37:11 +0700

    projek kel 6

--- pariwisata/detail_wisata.php
+++ pariwisata/detail_wisata.php
@@ -1,8 +1,8 @@
 <?php 
     include './backend/koneksi.php';
     $url = explode('/', $_SERVER["PHP_SELF"]);
-    $data = mysqli_query($connect, "SELECT * FROM tb_wisata WHERE slug = '$url[3]'");
-    while ($item = mysqli_fetch_array($data)) {
+    $data_wisata = mysqli_query($connect, "SELECT * FROM tb_wisata WHERE slug = '$url[3]'");
+    while ($item_wisata = mysqli_fetch_array($data_wisata)) {
             
 ?>
 <!--
@@ -29,7 +29,7 @@ Author URL: http://w3layouts.com
         <ul class="breadcrumbs-custom-path">
             <li><a href="/pariwisata/index.php">Home</a></li>
             <li><a href="/pariwisata/wisata.php"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> Wisata</a></li>
-            <li class="active"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> <?= $item['nama_wisata']; ?></li>
+            <li class="active"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> <?= $item_wisata['nama_wisata']; ?></li>
         </ul>
     </div>
 </section>
@@ -40,17 +40,28 @@ Author URL: http://w3layouts.com
             <div class="row mb-4">
                 <div class="col col-lg-2"></div>
                 <div class="col col-lg-8">
-                    <img src="/pariwisata/assets/images/<?php echo $item['gambar_wisata']; ?>" class="radius-image img-fluid w-100 h-100">
+                    <img src="/pariwisata/assets/images/<?php echo $item_wisata['gambar_wisata']; ?>" class="radius-image img-fluid w-100 h-100">
                 </div>
                 <div class="col col-lg-2"></div>
             </div>
             <div class="row">
                 <div class="col-lg-12 mt-lg-0 mt-md-5 mt-4 align-self">
-                    <h3 class="title-big mx-0"><?= $item['nama_wisata']; ?></h3>
-                    <p class="mt-md-4 mt-3 text-justify"><?= $item['deskripsi_wisata']; ?></p><br>
-                    <p class="mb-3"><span class="fa fa-money"></span> Rp.<?php echo number_format($item['harga_wisata'],2) ; ?></p><br>
-                    <p class="mb-3"><span class="fa fa-map-marker"></span> <?= $item['nama_wisata']; ?></p><br>
-                    <?= $item['alamat_wisata']; ?>
+                    <h3 class="title-big mx-0"><?= $item_wisata['nama_wisata']; ?></h3>
+                    <p class="mt-md-4 mt-3 text-justify"><?= $item_wisata['deskripsi_wisata']; ?></p><br>
+                    <p class="mb-3"></span> <h5 class="title-big mx-0">Harga : Rp.<?php echo number_format($item_wisata['harga_wisata'],2) ; ?></h5></p><br>
+                    
+                    <div class="row">
+                        <div class="col">
+                            <?= $item_wisata['alamat_wisata']; ?>    
+                        </div>
+                        <div class="col">
+                            <p class="mt-3">
+                                <span class="fa fa-map-marker"></span>&nbsp <?= $item_wisata['nama_wisata']; ?>
+                            </p>
+                         </div>
+                    </div>
+                    
+                    <br>
                 </div>
             </div>
         </div>
