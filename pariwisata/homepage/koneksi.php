76178c3beaba8ff164a97a3081bac7814d915471
IvonCsian <ivoncsianeeo@gmail.com>
2021-01-07 15:41:43 +0700

    f

--- pariwisata/homepage/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'db_pariwisata'; //sesuai nama databasemu//
-$connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die('koneksi gagal');
-?>
