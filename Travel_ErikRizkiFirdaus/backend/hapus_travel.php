2edf9bdba171bf222e1bd8e4f9854f9fa0f0ef2b
Erkan-s <72178769+Erkan-s@users.noreply.github.com>
2021-01-07 17:47:13 +0700

    Revert "Travel"
    This reverts commit 6591c73b5072c91e2ecdbdad26276363d3d9f612.

--- Travel_ErikRizkiFirdaus/backend/hapus_travel.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'koneksi.php';
-$id_travel = $_GET['id'];
-mysqli_query($connect,"delete from tb_travel where id_travel='$id_travel'");
-
-header("location:travel.php?pesan=hapus");
-?>
